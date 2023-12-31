8da6280a0a4fc2cfd5d3182da03e3a0e62be3114
Blue22One <146103106+Blue22One@users.noreply.github.com>
2023-12-31 11:12:35 +0200

    Regulator ZieglerNichols

---
+++ ZieglerNichols.m
@@ -0,0 +1,15 @@
+sim('proiect_ZieglerNichols.slx')
+
+figure
+plot(t, u_c)
+title('Grafic u_c')
+
+
+figure
+plot(t, theta)
+title('Grafic theta')
+
+
+figure
+plot(t, u_thetam)
+title('Grafic u_ thetam')
